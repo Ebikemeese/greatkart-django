0be043e5e13011cb47d09266fc1016fb10f457ce
Ebikemeese <ebikemeese257@gmail.com>
2025-06-04 08:28:06 +0100

    Completed Project

--- Store/views.py
+++ Store/views.py
@@ -5,7 +5,7 @@ from carts.models import cartItem
 from carts.views import _cart_id
 from django.http import HttpResponse
 from django.db.models import Q
-from .models import ReviewRating
+from .models import ReviewRating, ProductGallery
 from .forms import ReviewForm
 from django.contrib import messages
 from orders.models import OrderProduct
@@ -64,11 +64,15 @@ def product_detail(request, category_slug, product_slug):
     #get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    #Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
